501d567f1efd911b0f6fb01b194e481bf78b0e7f
hyunk-go <78598447+hyunk-go@users.noreply.github.com>
2021-09-23 14:24:47 +0900

    [브루트 포스] 9월 23일 -update
    20055번 회전없이 표현

--- 9월 21일/20055.cpp
+++ 9월 21일/20055.cpp
@@ -6,47 +6,66 @@ vector<pair<int, int>> a;
 vector<pair<int, int>> b;
 int cnt = 0;
 int n;
+int up;
+int down;
 
 void stepOne()
 {
-	pair<int, int> temp = a[2 * n];
+	/*pair<int, int> temp = a[2 * n];
 	for (int i = 2 * n; i >= 1; i--)
 		a[i] = a[i - 1];
 	a[1] = temp;
-	//이렇게 함으로써 한칸 회전했다.
-	if (a[n].second)
-		a[n].second = 0;//내릴곳에 로봇있으면 로봇 제거
+	//이렇게 함으로써 한칸 회전했다.*/
+	
+	up--;
+	down--;
+	if (up < 1)
+		up = 2 * n;
+	if (down < 1)
+		down = 2 * n;
+	
+	if (a[down].second)
+		a[down].second = 0;//내릴곳에 로봇있으면 로봇 제거
 
 }
 
 void stepTwo()
 {
-	for (int i = n - 1; i >= 1; i--)
+	int num = down;
+	int next;
+	while(1)
 	{
-		if (a[i].second)//로봇이있다면
+		next = num;
+		if (num == 1)
+			num = 2 * n;
+		else
+			num--;
+		if (a[num].second)//로봇이있다면
 		{
-			if (!a[i + 1].second && a[i + 1].first >= 1) //앞칸에 로봇없고 내구도 1이상일때 옮기기
+			if (!a[next].second && a[next].first >= 1) //앞칸에 로봇없고 내구도 1이상일때 옮기기
 			{
-				a[i + 1].second = 1;
-				a[i + 1].first--; //내구도 감소
-				a[i].second = 0; //로봇 이동했으니 사라짐
+				a[next].second = 1;
+				a[next].first--; //내구도 감소
+				a[num].second = 0; //로봇 이동했으니 사라짐
 
-				if (a[i + 1].first == 0)
+				if (a[next].first == 0)
 					cnt++;
 			}
 		}
+		if (num == up)
+			break;
 	}
-	if (a[n].second == 1)//다 끝나고 로봇 내릴거있음 내리기
-		a[n].second = 0;
+	if (a[down].second == 1)//다 끝나고 로봇 내릴거있음 내리기
+		a[down].second = 0;
 }
 
 void stepThree()
 {
-	if (a[1].first >= 1 && !a[1].second)
+	if (a[up].first >= 1 && !a[up].second)
 	{
-		a[1].first--;
-		a[1].second = 1;
-		if (a[1].first == 0)
+		a[up].first--;
+		a[up].second = 1;
+		if (a[up].first == 0)
 			cnt++;
 	}
 }
@@ -63,12 +82,12 @@ int main()
 	cin >> n >> k;
 	a.assign(201, { 0,0 });
 	b.assign(201, { 0,0 });
-
+	up = 1;
+	down = n;
 	for (int i = 1; i <= 2 * n; i++)
 	{
 		cin >> input;
 		a[i].first = input;
-
 	}
 	int t = 0;
 	
