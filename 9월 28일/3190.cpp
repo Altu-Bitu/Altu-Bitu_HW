6fd7321f93b83876ec7845db901aaba89425597f
hyunk-go <78598447+hyunk-go@users.noreply.github.com>
2021-10-01 00:55:25 +0900

    [동적 계획법] 10월 1일 -ing
    3190의 예제 3가 오류가 뜨는데 이유가 궁금합니다.

--- 9월 28일/3190.cpp
+++ 9월 28일/3190.cpp
@@ -1,31 +1,89 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
-struct snake
-{
-	int x;
-	char c;
-};
+
+bool body[101][101];
+int x[4] = { 0,1,0,-1 }; //회전할시 행의 변천사
+int y[4] = { 1,0,-1,0 }; //회전할시 열의 변천사 D를 기준으로 4개(반대로가면 L기준)
+
 int main()
 {
-	int n,k;
+	//입출력 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, k, r, c,t;
+	char d;
 	cin >> n >> k;
-	vector<pair<int, int>> apple;
-	apple.assign(k, { 0,0 });
+	deque<pair<int,int>> s;
+	bool apple[101][101] = { false };
+
 	for (int i = 0; i < k; i++)
 	{
-		cin >> apple[i].first >> apple[i].second;
-	}
-	int l;
-	vector<snake> s;
-	s.assign(l, { 0,' ' });
-	for (int i = 0; i < l; i++)
-	{
-		cin >> s[i].x >> s[i].c;
+		cin >> r >> c;
+		apple[r][c] = true;
 	}
 
+	int l;
+	cin >> l;
+	char time[10001];
+	for (int i = 0; i < l; i++)
+	{ 
+		cin >> t >> d;
+		time[t] = d;
+	}
+	 //입력 끝
+
+	s.push_back({ 1,1 });//처음 start위치
+	body[1][1] = true; //뱀이 존재해요
+	int times = 0;
+	int now = 0;
+	while (1)
+	{
+		times++;
+
+		int next_r = s.back().first + x[now]; //한칸 전진
+		int next_c = s.back().second+y[now];
+
+		if (next_r<1 || next_r>n || next_c<1 || next_c>n)
+			break;
+
+		s.push_back({ next_r,next_c }); //머리갱신
+
+		if (time[times] == 'D')
+		{
+			now += 1;
+			if (now == 4)
+				now = 0;
+		}
+
+		else if (time[times] == 'L')
+		{
+			now--;
+			if (now == -1)
+				now = 3;
+		}
+
+		if (apple[next_r][next_c])
+			apple[next_r][next_c] = false; //사과 냠냠
+		else
+		{
+			body[s.front().first][s.front().second] = false;
+			s.pop_front(); //꼬리 지우기
+		}
+
+		if (body[next_r][next_c])
+			break;
+		else
+			body[next_r][next_c] = true; //추가
+
+	}
+
+	cout << times;
 
 	return 0;
 }
