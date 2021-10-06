e93792a4c2edddef5cbce6709d5b175a0871a01e
hyunk-go <78598447+hyunk-go@users.noreply.github.com>
2021-10-06 22:26:26 +0900

    [그리디 알고리즘] 10월 6일 -ing
    주유소 문제 마지막 조건은 뭘까요?
    일단 둘다 반복문 하나로 간단하게 줄였어요!
    감사합니다:>

--- 10월 1일/13305.cpp
+++ 10월 1일/13305.cpp
@@ -5,71 +5,31 @@
 
 using namespace std;
 
-int cost(int n,int dist,vector<pair<int,int>> s,vector<int> sum)
-{
-	int total = 0;
-	int distance = 0;
-	int sums;
-	int standard;
-	for (int i = 0; i < n - 1; i++)
-	{
-		
-		if (i == 0)
-		{
-			sums = sum[s[i].second];
-			standard = i;
-		}
-		else if (s[i].second < s[standard].second)
-		{
-			sums = sum[s[i].second] - sum[s[standard].second];
-			standard = i;
-		}
-		else
-			continue;
-		if (sums == 0)
-			return total;
-		total += s[i].first * sums;
-		distance += sums;
-		if (distance == dist)
-			return total;
-	}
-}
-
-bool cmp(pair<int, int>& a, pair<int, int>& b)
-{
-	if (a.first != b.first)
-		return a.first < b.first;
-	return a.second < b.second;
-}
-
 int main()
 {
-	int n;
-	int flag = 0;
+	int n,input;
 	cin >> n;
 	vector<int> oil(n, 0);
 	vector<int> dist(n, 0);
-	vector<pair<int, int>> s(n - 1, { 0, 0});
+	
 	for (int i = 0; i < n-1; i++)
 		cin >>dist[i];
 	for (int i = 0; i < n; i++)
 	{
-		cin >> oil[i];
-		if (i > 0 && oil[i] != oil[i - 1])
-			flag = 1;
-		if (i != n - 1)
-			s[i] = { oil[i],i };
+		cin >> input;
+		if (i == 0)
+			oil[i] = input;
+		else
+		{
+			if (oil[i - 1] < input)
+				oil[i] = oil[i - 1];
+			else
+				oil[i] = input;
+		}
 	}
-
-	sort(s.begin(), s.end(),cmp);
-
-	vector<int> sum(n-1, 0);
+	int sum = 0;
 	for (int i = 0; i < n - 1; i++)
-	{
-		for (int j = i; j < n - 1; j++)
-			sum[i] +=dist[j];
-	}
-
-	cout << cost(n,sum[0],s, sum);
+		sum += dist[i] * oil[i];
+	cout << sum;
 	return 0;
 }
--- 10월 1일/1448.cpp
+++ 10월 1일/1448.cpp
@@ -18,28 +18,12 @@ int main()
 		cin >> tri[i];
 	sort(tri.begin(), tri.end(), greater<>());
 	int max_num = -1;
-	
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i <= n-3; i++) //어차피 그 앞에 맨 처음 세수만 해주면됨
 	{
-		int flag = 0;
-		for (int j = i + 1; j < n; j++)
+		if (checkTriangle(tri[i], tri[i + 1], tri[i + 2]))
 		{
-			for (int k = j + 1; k < n; k++)
-			{
-				if (checkTriangle(tri[i], tri[j], tri[k]))
-				{
-					max_num = max(max_num, tri[i] + tri[j] + tri[k]);
-					cout << max_num;
-					return 0;
-				}
-				else
-				{
-					flag = 1; //만약 적합하지 않은 삼각형 조합이면 바로 i 반복문으로 돌아가기
-					break;
-				}
-			}
-			if (flag)
-				break;
+			max_num = tri[i] + tri[i + 1] + tri[i + 2];
+			break;
 		}
 	}
 	cout << max_num;
