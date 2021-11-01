0361fbf41e0413e85b73b76074356be08b713142
hyunk-go <78598447+hyunk-go@users.noreply.github.com>
2021-11-01 19:04:47 +0900

    [DFS&BFS] 11월 1일 -update
    수정했습니다.

--- 10월 15일/1520.cpp
+++ 10월 15일/1520.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 
 int m, n;
-int matrix[501][501]; 
-int index[501][501];
+int matrix[501][501];
+vector<vector<int>> index(501, vector<int>(501, -1));
 
 int solution(int i, int j)
 {
@@ -18,14 +18,14 @@ int solution(int i, int j)
 		return 1; //1을 반환
 	if (index[i][j] == -1) //dp가 처음 접근하는 경우만 갱신
 	{
-		index[i][j] = 0; //더할수있게 값을 변경해주자+0이된순간 이 dp에는 접근을 했던것이라는것을 알려줌
+		index[i][j] = 0;
 		for (int k = 0; k < 4; k++) //상하좌우 돌면서 길 선택
 		{
 			int nr = i + dr[k]; //갱신
 			int nc = j + dc[k];
 			if (nr > 0 && nr <= m && nc > 0 && nc <= n && matrix[i][j] > matrix[nr][nc])
 				index[i][j] += solution(nr, nc);
-			
+
 		}
 	}
 	return index[i][j];
@@ -35,14 +35,7 @@ int main()
 {
 	cin >> m >> n;
 
-	for (int i = 0; i <= m; i++)
-	{
-		for (int j = 0; j <= n; j++)
-		{
-			matrix[i][j] = 10001; //index가 0일때 접근해도 값이 변경되지 않게 10001최대값으로 설정
-			index[i][j] = -1;
-		}
-	}
+
 	for (int i = 1; i <= m; i++)
 		for (int j = 1; j <= n; j++)
 			cin >> matrix[i][j];
